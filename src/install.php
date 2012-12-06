de3a051e811957dba90e5558fc85165a91a7ce7f
nguyenk <k.nguyen@thecodingmachine.com>
2012-12-06 10:34:57 +0100

    Creating a WebLibrary at install

--- src/install.php
+++ src/install.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__."/../../../autoload.php";
 
 use Mouf\MoufManager;
@@ -12,13 +11,27 @@ InstallUtils::init(InstallUtils::$INIT_APP);
 // Let's create the instance
 $moufManager = MoufManager::getMoufManager();
 if (!$moufManager->instanceExists("userMessageService")) {
-	$moufManager->declareComponent("userMessageService", "Mouf\Html\Widgets\MessageService\Service\SessionMessageService");
+	$moufManager->declareComponent("userMessageService", "Mouf\\Html\\Widgets\\MessageService\\Service\\SessionMessageService");
 }
 
 if (!$moufManager->instanceExists("messageWidget")) {
-	$moufManager->declareComponent("messageWidget", "Mouf\Html\Widgets\MessageService\Widget\MessageWidget");
+	$moufManager->declareComponent("messageWidget", "Mouf\\Html\\Widgets\\MessageService\\Widget\MessageWidget");
 	$moufManager->bindComponent("messageWidget", "messageProvider", "userMessageService");
 }
+
+if (!$moufManager->instanceExists("messageServiceLibrary")) {
+	$moufManager->declareComponent("messageServiceLibrary", "Mouf\\Html\\Utils\\WebLibraryManager\\WebLibrary");
+	$moufManager->bindComponent("messageWidget", "messageProvider", "userMessageService");
+}
+
+
+//Create a weblibraray for loading message services(s css file, and add it into the defaultWebLibrary Manager
+WebLibraryInstaller::installLibrary("messageServiceLibrary",
+	array(),
+	array('vendor/mouf/html.widgets.messageservice/messages.css'),
+	array(),
+	true
+);
 
 // Let's rewrite the MoufComponents.php file to save the component
 $moufManager->rewriteMouf();
